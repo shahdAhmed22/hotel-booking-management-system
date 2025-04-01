ea976f1d9b6181c725a36ab9ccb4e191a8e72963
shahdabouArb <shahed21-01621@student.eelu.edu.eg>
2025-04-01 19:20:58 +0200

    add :room router

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import { config } from "dotenv"
 import express from "express"
 import { DB_connection } from "./DB/connection.js"
 import userRouter from "./src/modules/auth/auth.routes.js"
-
+import roomRouter from "./src/modules/room/rooms.routes.js"
 config()//this function is enable us to use env variables that are in env file
 const app =express()
 const port = process.env.PORT ||3000
 app.use(express.json())
 
 app.use("/user",userRouter)
+app.use("/room",roomRouter)
 
 DB_connection()
 app.listen(port,()=>{
