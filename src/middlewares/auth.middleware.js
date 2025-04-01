4e66c3a21a5df30a83a1ee714732ed1ff0232036
shahdabouArb <shahed21-01621@student.eelu.edu.eg>
2025-04-01 10:32:27 +0200

    add: auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+import User from "../../DB/models/user.model.js";
+import { systemRoles } from "../utils/system-roles";
+export const auth=(authRoles=Object.values(systemRoles))=>{
+    return async (req,res,next)=>{
+        try{
+            const accessToken=req.headers.token;
+            if(!accessToken){
+                return res.status(404).json("please login first")
+            }
+            const decodedData=jwt.decode(accessToken,process.env.Verification_signature)
+            if(!decodedData||!decodedData.id){
+                return res.status(400).json({message:"invalied token payload"})
+            }
+            const user=await User.findById(id)
+            if(!user){
+                return res.status(404).json({message:"user not found"})
+            }
+            if(!authRoles.includes(user.role)){
+                return res.status(401).json({message:"you are not authorized to do that action"})
+            }
+            req.auth=user
+            next()
+        }catch(err){
+            return res.status(500).json({message:"error in auth middleware",error:err.message})
+        }
+    }
+}
